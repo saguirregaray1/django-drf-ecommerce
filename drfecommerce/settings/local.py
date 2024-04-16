ffe74c56451fca455e7bf59ee6e94f3ee3151f5f
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2024-04-16 13:05:14 -0300

    fix linting

--- drfecommerce/settings/local.py
+++ drfecommerce/settings/local.py
@@ -1,8 +1,8 @@
-from .base import *
+from .base import BASE_DIR
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
